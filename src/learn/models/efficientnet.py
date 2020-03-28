15f0a6af23c409b9d3a3c60b38d9307ceec59ce7
johSchm <johSchm@protonmail.com>
2020-03-28 22:22:09 +0100

    cleaned effnet

--- src/learn/models/efficientnet.py
+++ src/learn/models/efficientnet.py
@@ -5,6 +5,8 @@
 @author:     Johann Schmidt
 @date:       2020
 @refs:       https://github.com/tensorflow/tpu/blob/master/models/official/efficientnet/efficientnet_model.py
+             https://github.com/titu1994/keras-efficientnets
+             https://arxiv.org/abs/1905.11946
 @todo:
 @bug:
 @brief:
@@ -28,6 +30,8 @@ import learn.models.layers.swish as sw
 import learn.models.blocks.mbconvblock as mbb
 
 
+DEFAULT_DEPTH_DIVISOR = 8
+DEFAULT_MIN_DEPTH = 0
 DEFAULT_BATCH_NORM_MOMENTUM = 0.99
 DEFAULT_BATCH_NORM_EPSILON = 1e-3
 DEFAULT_DROP_CONNECT_RATE = 0.0
@@ -51,12 +55,8 @@ class VersionID(Enum):
 
 def get_width_coefficient(vid: VersionID) -> float:
     """ Returns the width coefficient for the specific model version:
-
-    Args:
-        vid (VersionID): Version ID.
-
-    Return:
-        width coefficient: float
+    :param vid: (VersionID) Version ID.
+    :return width coefficient: float
     """
     if type(vid) == VersionID:
         vid = vid.value
@@ -82,12 +82,8 @@ def get_width_coefficient(vid: VersionID) -> float:
 
 def get_depth_coefficient(vid: VersionID) -> float:
     """ Returns the depth coefficient for the specific model version:
-
-    Args:
-        vid (VersionID): Version ID.
-
-    Return:
-        depth coefficient: float
+    :param vid: (VersionID) Version ID.
+    :return: depth coefficient: float
     """
     if type(vid) == VersionID:
         vid = vid.value
@@ -123,28 +119,20 @@ def efficientnet_factory(
         log_path=None,
         ckpt_path=None,
         parallel=False,
-        layer_prefix="",
-        load_weights_after_logits=True):
+        layer_prefix=""):
     """ Init. method.
-
-    Args:
-        load_weights_after_logits (bool): Load the weights after adding the logits.
-        layer_prefix (str): Add this prefix to ALL layer names.
-        weight_links (dict): This dictionary contains the link to the weights.
-        input_shape (list): Input shape of the input data (W x H x D).
-        output_shape (list): The output shape for the output data.
-        optimizer (utils.Optimizer): The optimizer.
-        loss (utils.Loss): The loss.
-        metrics (utils.Metrics): The evaluation metric or metrics.
-        normalization (utils.Normalizations): The normalization method.
-        parallel (bool): Enable GPU parallelism.
-        log_path (str): The path to the desired log directory.
-        ckpt_path (str): The path to the checkpoint directory.
-        id (VersionID): The version ID of the model.
-        shape_format (list): The shape format.
-
-    Returns:
-        model (EfficientNet): The configured model.
+    :param layer_prefix: (str) Add this prefix to ALL layer names.
+    :param weight_links: (dict) This dictionary contains the link to the weights.
+    :param input_shape: (list) Input shape of the input data (W x H x D).
+    :param output_shape: (list) The output shape for the output data.
+    :param optimizer: (utils.Optimizer) The optimizer.
+    :param loss: (utils.Loss) The loss.
+    :param metrics: (utils.Metrics) The evaluation metric or metrics.
+    :param normalization: (utils.Normalizations) The normalization method.
+    :param log_path: (str) The path to the desired log directory.
+    :param ckpt_path: (str) The path to the checkpoint directory.
+    :param id: (VersionID) The version ID of the model.
+    :return model (EfficientNet): The configured model.
     """
     setup_dict = {}
     if id == VersionID.B0 or id == VersionID.B0.value:
@@ -161,7 +149,6 @@ def efficientnet_factory(
         ckpt_path=ckpt_path,
         parallel=parallel,
         layer_prefix=layer_prefix,
-        load_weights_after_logits=load_weights_after_logits,
         kwargs=setup_dict)
 
 
@@ -172,19 +159,18 @@ class EfficientNetConvInitializer(tf.keras.initializers.Initializer):
     standard deviation, whereas base_path we use a normal distribution. Similarly,
     tf.contrib.layers.variance_scaling_initializer uses a truncated normal with
     a corrected standard deviation.
-
-    # Arguments:
-      shape: shape of variable
-      dtype: dtype of variable
-      partition_info: unused
-
-    # Returns:
-      an initialization for the variable
     """
     def __init__(self):
+        """ Init. method.
+        """
         super(EfficientNetConvInitializer, self).__init__()
 
     def __call__(self, shape, dtype=None):
+        """ Call method.
+        :param shape:
+        :param dtype:
+        :return: an initialization for the variable
+        """
         dtype = dtype or tf.keras.backend.floatx()
 
         kernel_height, kernel_width, _, out_filters = shape
@@ -196,33 +182,28 @@ class EfficientNetConvInitializer(tf.keras.initializers.Initializer):
 class EfficientNetDenseInitializer(tf.keras.initializers.Initializer):
     """Initialization for dense kernels.
         This initialization is equal to
-          tf.variance_scaling_initializer(scale=1.0/3.0, mode='fan_out',
-                                          distribution='uniform').
+          tf.variance_scaling_initializer(scale=1.0/3.0, mode='fan_out', distribution='uniform').
         It is written out explicitly base_path for clarity.
-
-        # Arguments:
-          shape: shape of variable
-          dtype: dtype of variable
-          partition_info: unused
-
-        # Returns:
-          an initialization for the variable
     """
     def __init__(self):
+        """ Init. method.
+        """
         super(EfficientNetDenseInitializer, self).__init__()
 
     def __call__(self, shape, dtype=None):
+        """ Call method.
+        :param shape:
+        :param dtype:
+        :return: an initialization for the variable
+        """
         dtype = dtype or tf.keras.backend.floatx()
 
         init_range = 1.0 / np.sqrt(shape[1])
         return tf.keras.backend.random_uniform(shape, -init_range, init_range, dtype=dtype)
 
 
-
 class EfficientNet(model.Model):
     """ Efficient Net.
-    Code:   https://github.com/titu1994/keras-efficientnets
-    Paper:  https://arxiv.org/abs/1905.11946
     """
 
     def __init__(self,
@@ -236,23 +217,30 @@ class EfficientNet(model.Model):
                  ckpt_path=None,
                  layer_prefix="",
                  data_format=None,
+                 block_args=None,
+                 width_coefficient=0.0,
+                 depth_coefficient=0.0,
+                 batch_norm_momentum=DEFAULT_BATCH_NORM_MOMENTUM,
+                 batch_norm_epsilon=DEFAULT_BATCH_NORM_EPSILON,
+                 drop_connect_rate=DEFAULT_DROP_CONNECT_RATE,
                  **kwargs):
         """ Init. method.
-
-        Args:
-            data_format (list): The format of the image shape.
-            layer_prefix (str): Add this prefix to ALL layer names.
-            weight_links (dict): This dictionary contains the link to the weights.
-            input_shape (list): Input shape of the input data (W x H x D).
-            output_shape (list): The output shape for the output data.
-            optimizer (utils.Optimizer): The optimizer.
-            loss (utils.Loss): The loss.
-            metrics (utils.Metrics): The evaluation metric or metrics.
-            log_path (str): The path to the desired log directory.
-            ckpt_path (str): The path to the checkpoint directory.
-
-        Keyword Args:
-            block_args (list): Block arguments.
+        :param data_format (list): The format of the image shape.
+        :param layer_prefix (str): Add this prefix to ALL layer names.
+        :param weight_links (dict): This dictionary contains the link to the weights.
+        :param input_shape (list): Input shape of the input data (W x H x D).
+        :param output_shape (list): The output shape for the output data.
+        :param optimizer (utils.Optimizer): The optimizer.
+        :param loss (utils.Loss): The loss.
+        :param metrics (utils.Metrics): The evaluation metric or metrics.
+        :param log_path (str): The path to the desired log directory.
+        :param ckpt_path (str): The path to the checkpoint directory.
+        :param block_args (list): Block arguments.
+        :param width_coefficient (float):
+        :param depth_coefficient (float):
+        :param batch_norm_momentum (float):
+        :param batch_norm_epsilon (float):
+        :param drop_connect_rate (float):
         """
         super().__init__(
             input_shape=input_shape,
@@ -267,25 +255,12 @@ class EfficientNet(model.Model):
         self._block_args = []
         self._include_top = True
         self._dropout_rate = 0.0
-        if kwargs is not None:
-            if "block_args" in kwargs.keys():
-                self._block_args = None if kwargs["block_args"] is None else bargs.get_default_block_list()
-            if "width_coefficient" in kwargs.keys():
-                self._width_coefficient = kwargs["width_coefficient"]
-            if "depth_coefficient" in kwargs.keys():
-                self._depth_coefficient = kwargs["depth_coefficient"]
-            if "depth_divisor" in kwargs.keys():
-                self._depth_divisor = kwargs["depth_divisor"]
-            if "min_depth" in kwargs.keys():
-                self._min_depth = kwargs["min_depth"]
-            if "batch_norm_momentum" in kwargs.keys():
-                self._batch_norm_momentum = kwargs["batch_norm_momentum"]
-            if "batch_norm_epsilon" in kwargs.keys():
-                self._batch_norm_epsilon = kwargs["batch_norm_epsilon"]
-            if "drop_connect_rate" in kwargs.keys():
-                self._drop_connect_rate = kwargs["drop_connect_rate"]
-            if "pooling" in kwargs.keys():
-                self._pooling= kwargs["pooling"]
+        self._block_args = None if block_args is None else bargs.get_default_block_list()
+        self._width_coefficient = width_coefficient
+        self._depth_coefficient = depth_coefficient
+        self._batch_norm_momentum = batch_norm_momentum
+        self._batch_norm_epsilon = batch_norm_epsilon
+        self._drop_connect_rate = drop_connect_rate
         self._data_format = None if data_format is None else tf.keras.backend.image_data_format()
         self._channel_axis = 1 if self._data_format == 'channels_first' else -1
         self._layer_prefix = layer_prefix
@@ -293,6 +268,8 @@ class EfficientNet(model.Model):
         self._metrics = metrics
         self._loss = loss
         self._weight_links = weight_links
+        self._depth_divisor = DEFAULT_DEPTH_DIVISOR
+        self._min_depth = DEFAULT_MIN_DEPTH
         self._model = self._build_model()
         self._construct_model()
         self._configure(optimizer=optimizer, loss=loss, metrics=metrics)
@@ -306,12 +283,8 @@ class EfficientNet(model.Model):
 
     def _round_filters(self, filters):
         """ Rounds the number of filters if required.
-
-        Args:
-            filters (int): Number of original filters.
-
-        Returns:
-            rounded filters (int): Altered number of filters.
+        :param filters (int): Number of original filters.
+        :return rounded filters (int): Altered number of filters.
         """
         try:
             filters = utils.round_filters(
@@ -325,12 +298,7 @@ class EfficientNet(model.Model):
 
     def _construct_model(self) -> (tf.Tensor, tf.Tensor):
         """ Adds layers to the learn.
-
-        Args:
-            self (self): self
-
-        Returns:
-            None: None
+        :return input and output tensor
         """
         input_tensor = None
         if input_tensor is None:
@@ -427,16 +395,14 @@ class EfficientNet(model.Model):
 
     def _build_model(self) -> Model:
         """ Builds the model
-
-        Returns:
-            model: The final model.
+        :return model: The composed model.
         """
         model_in, model_out = self._construct_model()
         self._model = Model(inputs=model_in, outputs=model_out)
         return self._model
 
-    def _setup_pretrained_weights(self) -> Model:
-        """ Setup for pretrained weights.
+    def _setup_weights(self) -> Model:
+        """ Setup for weights.
         :return: model
         """
         if self._weight_links is None:
